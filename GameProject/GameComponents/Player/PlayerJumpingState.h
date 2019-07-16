554ee324b33bf06bd5b2dbcb197b7058f36399e8
Du Thai Cao <thai.caodu@gmail.com>
2019-07-16 18:20:28 +0700

    add more stages

---
+++ GameProject/GameComponents/Player/PlayerJumpingState.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Player.h"
+#include "PlayerState.h"
+
+class PlayerRunningState : public PlayerState
+{
+public:
+	PlayerRunningState();
+	// Inherited via PlayerState
+	virtual void Update(float deltaTime) override;
+
+	virtual void HandleKeyboard(std::map<int, bool> keys) override;
+
+	virtual StateName GetState() override;
+
+};

3260776df604306aebfbbb0da5b04944134176e7
Du Thai Cao <thai.caodu@gmail.com>
2019-08-09 00:55:05 +0700

    xong healthbar

--- GameProject/GameComponents/UI/HealthBar.cpp
+++ GameProject/GameComponents/UI/HealthBar.cpp
@@ -1,8 +1,8 @@
 #include "HealthBar.h"
 #include "../Player/Player.h"
 
-#define X_OFFSET 20
-#define Y_OFFSET -200
+#define X_OFFSET 40
+#define Y_OFFSET 40
 
 HealthBar::HealthBar()
 {
@@ -10,7 +10,6 @@ HealthBar::HealthBar()
 	isDead = false;
 	isVisible = true;
 	SetState(HB1);
-	GRID->AddObject(this);
 	
 }
 
@@ -21,37 +20,37 @@ void HealthBar::SetState(HBState state)
 	{
 		case HB1:
 		{
-			healthbarsprite->_sourceRect = { 0, 32, 8, 8 };
+			healthbarsprite->_sourceRect = { 0, 32, 8, 40 };
 			healthbarsprite->_textureWidth = width = 8;
 			healthbarsprite->_textureHeight = height = 8;
 			break;
 		}
 		case HB2:
 		{
-			healthbarsprite->_sourceRect = { 8, 24, 8, 8 };
+			healthbarsprite->_sourceRect = { 8, 24, 16, 40 };
 			healthbarsprite->_textureWidth = width = 8;
-			healthbarsprite->_textureHeight = height = 8;
+			healthbarsprite->_textureHeight = height = 16;
 			break;
 		}
 		case HB3:
 		{
-			healthbarsprite->_sourceRect = { 16, 16, 8, 8 };
+			healthbarsprite->_sourceRect = { 16, 16, 24, 40 };
 			healthbarsprite->_textureWidth = width = 8;
-			healthbarsprite->_textureHeight = height = 8;
+			healthbarsprite->_textureHeight = height = 24;
 			break;
 		}
 		case HB4:
 		{
-			healthbarsprite->_sourceRect = { 24, 8, 8, 8 };
+			healthbarsprite->_sourceRect = { 24, 8, 32, 40 };
 			healthbarsprite->_textureWidth = width = 8;
-			healthbarsprite->_textureHeight = height = 8;
+			healthbarsprite->_textureHeight = height = 32;
 			break;
 		}
 		case HB5:
 		{
-			healthbarsprite->_sourceRect = { 32, 0, 8, 8 };
+			healthbarsprite->_sourceRect = { 32, 0, 40, 40 };
 			healthbarsprite->_textureWidth = width = 8;
-			healthbarsprite->_textureHeight = height = 8;
+			healthbarsprite->_textureHeight = height = 40;
 			break;
 		}
 	}

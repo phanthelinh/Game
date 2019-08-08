c7c146d8b1bc29e6cb31ed868f0f202edf7dc500
Du Thai Cao <thai.caodu@gmail.com>
2019-08-08 21:40:18 +0700

    homing missle phien ban don gian

--- GameProject/GameComponents/Enemy/Weapon/HomingMissile.h
+++ GameProject/GameComponents/Enemy/Weapon/HomingMissile.h
@@ -22,13 +22,14 @@ public:
 	//posX, posY is the left, top of weapon
 	HomingMissile(float posX, float posY, int direction);
 
+	float Dot(float ax, float ay, float bx, float by);
+	float perpDot(float ax, float ay, float bx, float by);
 	void CalculateHoming();
 	void OnCollision(GameObject* object, float deltaTime = 0);
+	void Update(float deltaTime);
 	RECT GetBound();
 	BoundingBox GetBoundingBox();
-	void Update(float deltaTime);
 	void Draw(D3DXVECTOR3 position, D3DXVECTOR3 cameraPosition, RECT sourceRect = RECT(), D3DXVECTOR3 center = D3DXVECTOR3());
-	//Draw at its position
 	void Draw();
 	void Release();
 	void SetState(HomingState state);
